240260fe05cc07fea21d74cb85867d64a4d06298
heimdall <heimdall@c7512ed1-047c-6cfc-a765-d1b99ecde01f>
2012-03-15 15:23:47 +0000

    Add Parameters in rm_tmp.sh for qcronsub

--- toolkit/ChangeDetector/src/tmp/rm_tmp.sh
+++ toolkit/ChangeDetector/src/tmp/rm_tmp.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
-# deletes in .  tmp*.dump files older than 4 days
-find tmp*.dump -mtime +4 -exec rm {} \;
+#$ -l h_rt=0:05:00 
+#$ -l virtual_free=5M
+#$ -j y
+#$ -N RenderDeleteTmpDump
+#$ -wd /home/project/r/e/n/render/public_html/toolkit/ChangeDetector/src/tmp/
+# deletes tmp*.dump files older than 4 days
+find /home/project/r/e/n/render/public_html/toolkit/ChangeDetector/src/tmp/tmp*.dump -mtime +4 -exec rm {} \;
