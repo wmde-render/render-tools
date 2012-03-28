292148e2f916f9de8b1de6aa0c16634a1cd3dde5
heimdall <heimdall@c7512ed1-047c-6cfc-a765-d1b99ecde01f>
2012-03-28 09:36:05 +0000

    Changed tmp_construction Pipeline to create a mark file

--- toolkit/ChangeDetector/src/tmp/rm_tmp.sh
+++ toolkit/ChangeDetector/src/tmp/rm_tmp.sh
@@ -7,3 +7,15 @@
 # deletes tmp*.dump files older than 4 days
 find /home/project/r/e/n/render/public_html/toolkit/ChangeDetector/src/tmp/tmp*.dump -mtime +4 -exec rm {} \;
 
+day=`/bin/date +%Y%m%d`
+day=`expr $day - 1`
+
+filename_end=All1234.dump
+
+filestring=tmp_$day$filename_end
+result_file=tmp_$day.ok.dump
+
+if [ -f $filestring ]
+ then echo "$filestring ist da"  | cat > $result_file
+fi
+
