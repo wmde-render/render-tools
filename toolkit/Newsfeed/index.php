e635810b1e24b65634abd3f0cd557e9903c862b5
knissen <knissen@c7512ed1-047c-6cfc-a765-d1b99ecde01f>
2012-10-15 10:24:48 +0000

    corrected paths

--- toolkit/Newsfeed/index.php
+++ toolkit/Newsfeed/index.php
@@ -1,7 +1,6 @@
 <?php
-//LEA index.php
 $ts_pw = posix_getpwuid(posix_getuid());
-include( $ts_pw['dir'] . "/render-code/toolkit/pages/Tablehead.php" );
-include( $ts_pw['dir'] . "/render-code/toolkit/pages/inc/lang/" . $_SESSION["lang"] . "/newsfeed_txt.inc.php" );
-include( $ts_pw['dir'] . "/render-code/toolkit/Newsfeed/src/jsi-newsfeed.php" );
-include( $ts_pw['dir'] . "/render-code/toolkit/pages/Tablefoot.php" );
+include( $ts_pw['dir'] . "/public_html/toolkit/pages/Tablehead.php" );
+include( $ts_pw['dir'] . "/public_html/toolkit/pages/inc/lang/" . $_SESSION["lang"] . "/newsfeed_txt.inc.php" );
+include( $ts_pw['dir'] . "/public_html/toolkit/Newsfeed/src/jsi-newsfeed.php" );
+include( $ts_pw['dir'] . "/public_html/toolkit/pages/Tablefoot.php" );
