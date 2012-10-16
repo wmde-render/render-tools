0104dda30d969395b0183b739547ad60f6f548db
gerritholz <gerritholz@c7512ed1-047c-6cfc-a765-d1b99ecde01f>
2012-10-16 14:23:52 +0000

    Beschreibung/Description fuer DE/EN hinzugefuegt

--- toolkit/pages/inc/lang/de/newsfeed_txt.inc.php
+++ toolkit/pages/inc/lang/de/newsfeed_txt.inc.php
@@ -1,7 +1,7 @@
 <?php
 $txtNewsfeed = array(
 	"headline" => "JSI Newsfeed",
-	"description" => "Beschreibung",
+	"description" => "Hier können über den <a href=\"http://newsfeed.ijs.si/\">Newsfeed von JSI</a> Nachrichten zu einem bestimmten Wikipedia-Artikel angefragt werden. Die Nachrichten stammen hauptsächlich aus Quellen in englischer Sprache. Das Tool befindet sich noch in der Entwicklung.",
 	"descTitle" => "Bitte einen Artikeltitel angeben",
 	"labelTitle" => "Artikeltitel:",
 	"btnSearch" => "Suchen",
