278a8bd96c8588907ccaca0591f88a797f6c21e9
render <render@c7512ed1-047c-6cfc-a765-d1b99ecde01f>
2012-11-20 20:34:54 +0000

    render account db config for wikigini

--- toolkit/WIKIGINI/inc/src/db.inc.php
+++ toolkit/WIKIGINI/inc/src/db.inc.php
@@ -2,7 +2,7 @@
 $ts_pw = posix_getpwuid(posix_getuid());
 $ts_mycnf = parse_ini_file($ts_pw['dir'] . "/.my.cnf");
 
-$mysqli = new mysqli('sql.toolserver.org', $ts_mycnf['user'], $ts_mycnf['password'], "u_fekepp_wikigini");
+$mysqli = new mysqli('sql.toolserver.org', $ts_mycnf['user'], $ts_mycnf['password'], "p_render_wikigini_p");
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: " . $mysqli->connect_error;
 }
