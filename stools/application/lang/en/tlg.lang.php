b59fae3a6deb4b5c0040cd758779ee40eaf5eba4
knissen <knissen@c7512ed1-047c-6cfc-a765-d1b99ecde01f>
2013-06-12 15:24:04 +0000

    textual correction

--- stools/application/lang/en/tlg.lang.php
+++ stools/application/lang/en/tlg.lang.php
@@ -3,8 +3,8 @@ $strLang = array(
 	"topHeading" => "Task List Generator",
 	"tlgDescription" => "This tool provides the ability to search Wikipedia 
 		categories with a given search depth. The <a href=\"http://en.wikipedia.org/wiki/Intersection_%28set_theory%29\">
-		intersection</a> and <a href=\"http://en.wikipedia.org/wiki/Intersection_%28set_theory%29\">
-		intersection</a> of categories can be created. The choice of filters decides 
+		intersection</a> and <a href=\"http://en.wikipedia.org/wiki/Set_difference#Relative_complement\">
+		difference</a> of categories can be created. The choice of filters decides 
 		which articles should be displayed.",
 	"descHeading" => "Language, Categories and Search Depth",
 	"descLanguage" => "Language",
