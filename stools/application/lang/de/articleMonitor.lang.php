bbf5bac484b19c1c7bd6de5abb85c0446645cd30
knissen <knissen@c7512ed1-047c-6cfc-a765-d1b99ecde01f>
2013-06-13 10:26:50 +0000

    language files for article monitor

---
+++ stools/application/lang/de/articleMonitor.lang.php
@@ -0,0 +1,140 @@
+<?php
+$strLang = array(
+	"dateFormat" => "d.m.Y H:i",
+	"articleMonitorHeading" => "Artikelmonitor",
+	"articleMonitorDesc" => "Der Artikelmonitor bietet einen Überlick über statistische 
+		Daten und verschiedene Analysen zu einem Wikipedia-Artikel mit deren 
+		Hilfe die Qualität des Artikels eingeschätzt werden kann.
+		<br /><br />
+		Nach erfolgter Installation erscheint neben der Schaltfläche zur 
+		Versionshistorie eine weitere mit der Aufschrift Artikelmonitor. Durch 
+		Klicken auf diese Schalftfläce öffnet sich ein Extrafenster, in dem die 
+		Statistiken und Analysen zusammengefasst sind.",
+	"instHeading" => "Installation",
+	"instDesc" => "Das Helferlein kann auf der Unterseite 'vector.js' der eigenen 
+		Benutzerseite installiert werden. Ist das automatische Login in die 
+		Wikipedia aktiviert, kann folgender Link verwendet werden, um die Seite 
+		zur Skripteinbindung aufzurufen:
+		<a href=\"http://de.wikipedia.org/wiki/Spezial:Meine Benutzerseite/common.js\" target=\"_blank\">
+			http://de.wikipedia.org/wiki/Spezial:Meine Benutzerseite/common.js
+		</a>
+		<br />
+		Weitere Informationen zur Individualisierung der Benutzeroberfläche in der 
+		Wikipedia gibt es auf der 
+		<a href=\"http://de.wikipedia.org/wiki/Wikipedia:Technik/Skin\" target=\"_blank\">Projektseite</a>.<br /><br /><br />
+		Zur Benutzung wird folgender Code benötigt:",
+	"noticeHeading" => "Hinweis",
+	"notice" => "Zur Evaluation der Nutzung enthält der Code die Variable 
+		'asqmid'. Der Wert dient lediglich statistischen Zwecken und lässt 
+		keinen Rückschluss auf persönliche Angaben wie Benutzernamen zu.",
+	"newsfeedHeading" => "IJS Newsfeed",
+	"newsfeedDesc" => "Die Ergebnisse, die hier angezeigt werden, stammen aus 
+		dem Newsfeed des Josef-Stefan-Instituts in Ljubljana, Slowenien.<br />
+		Das Institut ist ein Partner des RENDER-Projekts.",
+	"newsfeedTableTitle" => "Titel",
+	"newsfeedTableUrl" => "URL",
+	"newsfeedTableDate" => "Datum",
+	
+	"articleMonitorTabTitle" => "Info",
+	"articleMonitorTooltip" => "Statistiken und weitere Analysen zu diesem Artikel anzeigen",
+	
+	# articleMonitor output
+	# old format - keep for now
+	"general" => array(
+		"title" => "Statistiken",
+		"pageTitle" => "Seitentitel",
+		"status" => "Status",
+		"firstEdit" => "Angelegt am",
+		"recentEdit" => "Letzte Änderung",
+		"editedBy" => "von",
+		"unregUsersPage" => "https://de.wikipedia.org/wiki/Hilfe:Benutzer#Nicht_angemeldeter_Benutzer",
+		"totalEditors" => "Autoren",
+		"references" => "Einzelnachweise",
+		"images" => "Mediendateien",
+		"visitorsToday" => "Besucher heute",
+		"visitors30days" => "Letzte 30 Tage",
+		"visitorsYesterday" => "Besucher gestern",
+		"visitorsLastMonth" => "Besucher im letzten Monat"
+	),
+	"factCoverage" => array(
+		"title" => "Faktenabdeckung",
+		"lea" => "LEA (Link Extractor)",
+		"showAnalysis" => "Analyse anzeigen",
+	),
+	"currentness" => array(
+		"title" => "Aktualität",
+		"titleNewsFinder" => "NewsFinder",
+		"noNews" => "Keine Nachrichtenartikel gefunden",
+		"newsFound" => " Nachrichtenartikel gefunden",
+		"titleChangeDetector" => "Change Detector",
+		"cdHit" => "Hohe Aktivität in anderen Sprachen",
+	),
+/*
+		"neutrality" => "Neutralität",
+		"editorInteraction" => "Autoreninteraktion",
+*/
+	"editorInteraction" => array(
+		"title" => "Autoreninteraktion",
+		"giniScore" => "WikiGini-Score",
+	),
+	"other" => array(
+		"title" => "Weitere Bewertungen",
+		"wikibuch" => "Wikibu.ch",
+		"lookupAssessment" => "Bewertung ansehen",
+	),
+	"aft4" => array(
+		"title" => "Article Feedback Score",
+		"trustworthy" => "Vertrauenswürdig",
+		"objective" => "Objektiv",
+		"complete" => "Vollständig",
+		"wellWritten" => "Gut geschrieben",
+	),
+	"aft5" => array(
+		"title" => "Article Feedback Score",
+		"negRating" => "Negative Bewertungen",
+	),
+	"statistics" => array(
+		"title" => "Statistiken",
+		"pageTitle" => "Seitentitel",
+		"status" => "Status",
+		"firstEdit" => "Angelegt am",
+		"recentEdit" => "Letzte Änderung",
+		"editedBy" => "von",
+		"totalEditors" => "Autoren",
+		"references" => "Einzelnachweise",
+		"images" => "Mediendateien",
+		"visitorsToday" => "Besucher heute",
+		"visitors30days" => "Letzte 30 Tage",
+		"visitorsYesterday" => "Besucher gestern",
+		"visitorsLastMonth" => "Besucher im letzten Monat"
+	),
+	"analysis" => array(
+		"title" => "Analysen",
+		# Link Extractor
+		"lea" => "Link Extractor",
+		"showAnalysis" => "Analyse anzeigen",
+		# News Finder
+		"titleNewsFinder" => "NewsFinder",
+		"noNews" => "Keine Nachrichtenartikel gefunden",
+		"newsFound" => " Nachrichtenartikel gefunden",
+		# Change Detector
+		"titleChangeDetector" => "Change Detector",
+		"cdHit" => "Hohe Aktivität in anderen Sprachen",
+		# WikiGini
+		"giniScore" => "WikiGini-Wert",
+	),
+	"assessment" => array(
+		"title" => "Bewertungen",
+		"wikibuch" => "Wikibu.ch",
+		"lookupAssessment" => "Bewertung ansehen",
+		# aft 4
+		"aft4title" => "Article Feedback Score",
+		"trustworthy" => "Vertrauenswürdig",
+		"objective" => "Objektiv",
+		"complete" => "Vollständig",
+		"wellWritten" => "Gut geschrieben",
+		# aft 5
+		"aft5title" => "Article Feedback Score",
+		"negRating" => "Negative Bewertungen",
+	),
+);
---
+++ stools/application/lang/en/articleMonitor.lang.php
@@ -0,0 +1,146 @@
+<?php
+$strLang = array(
+	"dateFormat" => "Y-m-d H:i",
+	"articleMonitorHeading" => "Article Monitor",
+	"articleMonitorDesc" => "The Article Monitor gives an overview of statistic data und 
+		various analysis for a Wikipedia article. You can use these to estimate 
+		the quality of an article.<br /><br />
+		After installing the gadget you will find a tab named \"Article Monitor\" 
+		besides the tab \"View history\". If you click on the tab, a popup window 
+		appears which displays the statistics and analysis referring to the current 
+		article",
+	"instHeading" => "Installation",
+	"instDesc" => "The gadget can be installed using the subpage 'vector.js' of 
+		a user's page. Having activated the automatic login to the Wikipedia, the 
+		following link can be used to install the gadget: 
+		<a href=\"http://de.wikipedia.org/wiki/Spezial:Meine Benutzerseite/common.js\" target=\"_blank\">
+			http://de.wikipedia.org/wiki/Special:MyPage/common.js
+		</a>
+		<br />
+		Further information on how to customize the user interface of the Wikipedia
+		is available on the 
+		<a href=\"http://de.wikipedia.org/wiki/Wikipedia:Technik/Skin\" target=\"_blank\">Project Page</a>.<br /><br /><br />
+		To make use of the gadget the following code is required:",
+	"noticeHeading" => "Notice",
+	"notice" => "For evaluation purposes the code contains the variable 'asqmid'.
+		This value has a statistical intent and cannot be linked to  personal 
+		data such as the username.",
+	"newsfeedHeading" => "IJS Newsfeed",
+	"newsfeedDesc" => "The result shown here are retrieved from the newsfeed 
+		provided by the Institute Josef-Stefan in Ljubljana, Slovenia.<br />
+		The institute is participating the RENDER Project.",
+	"newsfeedTableTitle" => "Title",
+	"newsfeedTableUrl" => "URL",
+	"newsfeedTableDate" => "Date",
+	
+	"articleMonitorTabTitle" => "Info",
+	"articleMonitorTooltip" => "Show statistics and further analysis for this article",
+	
+	# articleMonitor output
+	"general" => array(
+		"title" => "Statistics",
+		"pageTitle" => "Page Title",
+		"status" => "Status",
+		"firstEdit" => "Created",
+		"recentEdit" => "Recent Edit",
+		"editedBy" => "by",
+		"unregUsersPage" => "https://en.wikipedia.org/wiki/Wikipedia:User_access_levels#Unregistered_users",
+		"totalEditors" => "Editors",
+		"references" => "References",
+		"images" => "Media Files",
+		"visitorsToday" => "Visitors today",
+		"visitors30days" => "Last 30 days",
+		"visitorsYesterday" => "Visitors yesterday",
+		"visitorsLastMonth" => "Visitors last month"
+	),
+	"factCoverage" => array(
+		"title" => "Fact coverage",
+		"lea" => "Link Extractor",
+		"showAnalysis" => "Show Analysis",
+	),
+	"currentness" => array(
+		"title" => "Currentness",
+		"titleNewsFinder" => "News Finder",
+		"noNews" => "No news articles found",
+		"newsFound" => " news articles found",
+		"titleChangeDetector" => "Change Detector",
+		"cdHit" => "Detected as possibly outdated"
+	),
+	"editorInteraction" => array(
+		"title" => "Editor Interaction",
+		"giniScore" => "WikiGini Score",
+	),
+/*
+	"groupRender" => array(
+		"title" => "RENDER-Analysis",
+		"factCoverage" => "Fact Coverage",
+		"askLea" => "Ask LEA",
+		"neutrality" => "Neutrality",
+		"currentness" => "Currentness",
+		"editorInteraction" => "Editor Interaction",
+		"noNews" => "No news articles found",
+		"newsFound" => " news articles found",
+	),
+*/
+	"other" => array(
+		"title" => "Further Assessment",
+		"wikibuch" => "Wikibu.ch",
+		"lookupAssessment" => "Look up Assessment",
+	),
+	"aft4" => array(
+		"title" => "Article Feedback Score",
+		"trustworthy" => "Trustworthy",
+		"objective" => "Objective",
+		"complete" => "Complete",
+		"wellWritten" => "Well-written",
+	),
+	"aft5" => array(
+		"title" => "Article Feedback Score",
+		"negRating" => "Negative Ratings",
+	),
+	"statistics" => array(
+		"title" => "Statistics",
+		"pageTitle" => "Page Title",
+		"status" => "Status",
+		"firstEdit" => "Created",
+		"recentEdit" => "Recent Edit",
+		"editedBy" => "by",
+		"unregUsersPage" => "https://en.wikipedia.org/wiki/Wikipedia:User_access_levels#Unregistered_users",
+		"totalEditors" => "Editors",
+		"references" => "References",
+		"images" => "Media Files",
+		"visitorsToday" => "Visitors today",
+		"visitors30days" => "Last 30 days",
+		"visitorsYesterday" => "Visitors yesterday",
+		"visitorsLastMonth" => "Visitors last month",
+	),
+	"analysis" => array(
+		"title" => "Analysis",
+		# Link Extractor
+		"lea" => "Link Extractor",
+		"showAnalysis" => "Show analysis",
+		# News Finder
+		"titleNewsFinder" => "News Finder",
+		"noNews" => "No news articles found",
+		"newsFound" => " news articles found",
+		# Change Detector
+		"titleChangeDetector" => "Change Detector",
+		"cdHit" => "Detected as possibly outdated",
+		# WikiGini
+		"giniScore" => "WikiGini Score",
+	),
+	"assessment" => array(
+		"title" => "Assessments",
+		"wikibuch" => "Wikibu.ch",
+		"lookupAssessment" => "Lookup assessment",
+		# aft 4
+		"aft4title" => "Article Feedback Score",
+		"trustworthy" => "Trustworthy",
+		"objective" => "Objectivity",
+		"complete" => "Completeness",
+		"wellWritten" => "Well-written",
+		# aft 5
+		"aft5title" => "Article Feedback Score",
+		"negRating" => "negative ratings",
+	),
+);
