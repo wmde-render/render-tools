3cc815139ab5ea41ed91addcd684cfbbc77fc75d
Kai Nissen (WMDE) <kai.nissen@wikimedia.de>
2013-06-17 18:50:52 +0200

    blah...

--- toolkit/WIKIGINI/inc/src/db.inc.php
+++ toolkit/WIKIGINI/inc/src/db.inc.php
@@ -2,7 +2,7 @@
 $ts_pw = posix_getpwuid(posix_getuid());
 $ts_mycnf = parse_ini_file($ts_pw['dir'] . "/replica.my.cnf");
 
-$mysqli = new mysqli('tools-db', $ts_mycnf['user'], $ts_mycnf['password'], "p50380g50454__wikigini");
+$mysqli = new mysqli('tools-db', $ts_mycnf['user'], $ts_mycnf['password'], $ts_mycnf['user'] . "__wikigini");
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: " . $mysqli->connect_error;
 }
