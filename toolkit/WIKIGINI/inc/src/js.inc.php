6193fc771a8b16e10f0fd0cb62e6091805159209
Kai Nissen (WMDE) <kai.nissen@wikimedia.de>
2013-06-19 17:47:43 +0200

    load gini data from different server

--- toolkit/WIKIGINI/inc/src/js.inc.php
+++ toolkit/WIKIGINI/inc/src/js.inc.php
@@ -51,78 +51,7 @@ if ($load_graph) {
 	echo "\t\t\tmode = '" . $mode . "';\n";
 	echo "\t\t\tlanguage_code = '" . $language_code . "';\n";
 	echo "\t\t\tpage_id = '" . $page_id . "';\n";
-	echo "\t\t\tpage_title = '" . $page_title . "';\n";
-
-	echo "\t\t\twikigini_data = [['datetime'], ['revisions'], ['info']];\n";
-
-	$query = "SELECT
-id,
-unix_timestamp(datetime) * 1000 AS datetime,
-ROUND(gini_index, 12) AS gini
-FROM revisions WHERE method_id=1 AND language_code=\"" . $language_code . "\" AND page_id=" . $page_id . " ORDER BY datetime ASC";
-
-	$res = $mysqli->query($query);
-
-	$counter = 0;
-	$res->data_seek(0);
-	echo "\t\t\twikigini_data['datetime'] = [\n\t\t\t\t";
-	while ($row = $res->fetch_assoc()) {
-		echo "[" . $row["datetime"] . "," . $row["gini"] . "]" . ($counter < $res->num_rows - 1 ? "," : "");
-		$counter++;
-	}
-	echo "\n\t\t\t];\n";
-
-	$counter = 0;
-	$res->data_seek(0);
-	echo "\t\t\twikigini_data['datetime_info'] = [\n\t\t\t\t";
-	while ($row = $res->fetch_assoc()) {
-		echo "[" . $row["datetime"] . "]" . ($counter < $res->num_rows - 1 ? "," : "");
-		$counter++;
-	}
-	echo "\n\t\t\t];\n";
-
-	$query = "SELECT
-id,
-unix_timestamp(datetime) * 1000 AS datetime,
-ROUND(gini_index, 12) AS gini
-FROM revisions WHERE method_id=1 AND language_code=\"" . $language_code . "\" AND page_id=" . $page_id . " ORDER BY id ASC";
-
-	$res = $mysqli->query($query);
-
-	$counter = 0;
-	$res->data_seek(0);
-	echo "\t\t\twikigini_data['revisions'] = [\n\t\t\t\t";
-	while ($row = $res->fetch_assoc()) {
-		echo "[" . $row["id"] . "," . $row["gini"] . "]" . ($counter < $res->num_rows - 1 ? "," : "");
-		$counter++;
-	}
-	echo "\n\t\t\t];\n";
-
-	$counter = 0;
-	$res->data_seek(0);
-	echo "\t\t\t";
-	while ($row = $res->fetch_assoc()) {
-	//echo "\t\t\twikigini_data['datetime_info'] = [[" . $row["datetime"] . "];";
-	echo "wikigini_data['datetime_info'][" . $row["datetime"] . "] = [" . $row["id"] . ", " . $row["gini"] . "];";
-	}
-
-	$counter = 0;
-	$res->data_seek(0);
-	echo "\t\t\twikigini_data['revisions_info'] = [\n\t\t\t\t";
-	while ($row = $res->fetch_assoc()) {
-		echo "[" . $row["id"] . "]" . ($counter < $res->num_rows - 1 ? "," : "");
-		$counter++;
-	}
-	echo "\n\t\t\t];\n";
-
-	$counter = 0;
-	$res->data_seek(0);
-	echo "\t\t\t";
-	while ($row = $res->fetch_assoc()) {
-	//echo "\t\t\twikigini_data['datetime_info'] = [[" . $row["datetime"] . "];";
-	echo "wikigini_data['revisions_info'][" . $row["id"] . "] = [" . $row["datetime"] . ", " . $row["gini"] . "];";
-	}
-echo "\n";
+	echo file_get_contents('http://wikigini.fekepp.net/data.php?language=' . $language_code . '&pageid=' . $page_id);
 ?>
 		</script>
 		<script type="text/javascript" src="/<?php echo $tsAccount; ?>/toolkit/WIKIGINI/js/highcharts.js"></script>
